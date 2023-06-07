e5d67fa5dca4855feb322e8c0f9b295af8672f12
Sameer Zaher <sameerzaher@gmail.com>
2023-06-07 12:22:35 +0300

    My Timer Full App And Put the CarServiceApp in New Project

--- MyTimer/App.tsx
+++ MyTimer/App.tsx
@@ -1,104 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   Image,
+  Pressable,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  TextInput,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import LoginScreen from './src/screens/Login';
+import TimerScreen from './src/screens/Timer';
 function App(): JSX.Element {
-  const backgroundStyle = {
-    backgroundColor: '#F6FAF6',
-    flex: 1,
-    borderWidth: 2,
-  };
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <View style={styles.headerContainer}>
-        <View style={styles.LogoContainer}>
-          <Image source={require('./src/logoMyCar.png')} style={styles.Logo} />
-        </View>
-        
-      </View>
+    <SafeAreaView style={styles.safeAreaViewBase}>
       <View style={styles.rootContainer}>
-        <View style={styles.maincontainer}>
-          <Text style={styles.labelTXT}>
-            To Login to MyCar App Please enter Email Address and your Phone Number
-          </Text>
-          <View style={styles.sectionStyle}>
-  <TextInput
-    style={styles.input}
-    placeholder="Enter Your Email Here"
-    underlineColorAndroid="transparent"
-  />
-  <Image
-    source={require('./src/callSquare.png')}
-    style={styles.imageStyle}
-  />
-</View>
-<View style={styles.sectionStyle}>
-  <TextInput
-    style={styles.input}
-    placeholder="Enter Your Phone Here"
-    underlineColorAndroid="transparent"
-  />
-  <Image
-    source={require('./src/messageSquare.png')}
-    style={styles.imageStyle}
-  />
-</View>
-
+        <View style={styles.headerContainer}>
+          <View style={styles.logoContainer}>
+            <Image
+              style={styles.logo}
+              resizeMode={'contain'}
+              source={require('./src/assets/icons/logo.png')}
+            />
+          </View>
+          <Pressable
+            onPress={() => isAuthorized && setIsAuthorized(false)}
+            style={styles.headerTextContainer}>
+            <Text style={styles.logoText}>{'My Timer'}</Text>
+          </Pressable>
+          <View style={styles.rightContainer} />
         </View>
-      </View>
-      <View style={[styles.footerContainer]}>
-        <Image
-          source={require('./src/login-button-arrow.png')}
-          style={styles.footerImage}
-        />
-        <View style={styles.footerTextContainer}>
-          <Text style={styles.footerText}>Need Help</Text>
-          <Text style={styles.footerText2}>Contact Our Support</Text>
+        <View style={styles.bodyContainer}>
+          {isAuthorized ? (
+            <TimerScreen />
+          ) : (
+            <LoginScreen setIsAuthorized={setIsAuthorized} />
+          )}
         </View>
       </View>
     </SafeAreaView>
   );
 }
-const windowsHight = Dimensions.get('window').height;
+
+const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
+  bodyContainer: {flex: 9},
+  safeAreaViewBase: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
-    width: '100%',
+    backgroundColor: 'white',
     alignItems: 'center',
-    backgroundColor: '#F6FAF6',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   headerContainer: {
     width: '100%',
-    height: windowsHight * 0.1,
+    height: windowHeight * 0.1,
     flexDirection: 'row',
     alignItems: 'center',
     elevation: 2,
@@ -107,97 +67,31 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 10,
     borderColor: 'white',
-    justifyContent: 'space-between',
-    backgroundColor: '#F6FAF6',
-
+    backgroundColor: 'white',
   },
- 
-  sectionStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderWidth: 0.5,
-    borderColor: '#000',
-    height: 40,
-    borderRadius: 5,
-    margin: 10,
-  },
-  imageStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: 'stretch',
-    alignItems: 'center',
-    
-  },
-  LogoContainer: {
+  headerTextContainer: {
+    flex: 4,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 20,
   },
-  Logo: {
-    width: 200,
-    height: 50,
-    resizeMode: 'contain',
-  },
-  
-  
-  maincontainer: {
+  rightContainer: {
     flex: 1,
-    alignItems: 'center',
-    marginTop: 100,
-    width: '100%',
-    paddingHorizontal: 30,
   },
-  labelTXT: {
-    width: 100,
-    height: 50,
-    minWidth: 250,
-    fontFamily: 'Cochin',
-    fontSize: 15,
+  logoText: {
     color: 'black',
-    fontWeight: 'bold',
-
+    fontSize: 35,
+    fontWeight: '900',
   },
-  input: {
-    flex: 1,
-    height: 40,
-    marginVertical: 10,
-    padding: 10,
-    
-    minWidth: 250,
-  },
-  footerContainer: {
+  logo: {
     width: '100%',
-    height: 100,
-    flexDirection: 'row-reverse',
-    alignItems: 'center',
-    backgroundColor: '#F6FAF6',
-    marginBottom: 60,
+    height: '100%',
   },
-  footerImage: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#6b00b3',
-    marginHorizontal: 20,
-    borderRadius: 60 / 2,
-    transform: [{ rotate: '180deg' }],
-  },
-  footerTextContainer: {
+  logoContainer: {
     flex: 1,
-  },
-  footerText: {
-    fontSize: 16,
-    marginRight: 30,
-    fontWeight: 'bold',
-  },
-  footerText2: {
-    fontSize: 16,
-    marginRight: 30,
-    fontWeight: 'bold',
-    color: 'blue',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
